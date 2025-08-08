7f01a776ed7462d25e7bd13806d75a59a6492a1e
Sam Clark <sam.clark@posit.co>
2025-08-08 08:06:08 -0500

    Updates OIDC Profile model

--- auth.py
+++ auth.py
@@ -85,11 +85,7 @@ class EnsureAuthenticatedMiddleware(BaseHTTPMiddleware):
 
 class UserInfo(pydantic.BaseModel):
     sub: str
-    email_verified: bool
     name: str
-    preferred_username: str
-    given_name: str
-    family_name: str
     email: str
 
 
