3d9e1325b25fb03bb4294a9e34706aafb8c9a456
Sam Clark <sam.clark@posit.co>
2025-08-13 21:44:56 -0500

    Updates for more secure server

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 COPY . .
 EXPOSE 8080
-CMD ["python3", "nice.py"]
+CMD ["python3", "nice_public.py"]
---
+++ nice_public.py
@@ -0,0 +1,28 @@
+import os
+
+from dotenv import load_dotenv
+from nicegui import app, ui
+
+from auth import enable_oauth
+from pages import register_pages
+from startup import initialize
+
+load_dotenv()
+
+
+if "AUTH_ENABLED" in os.environ:
+    enable_oauth()
+
+app.on_startup(initialize)
+
+register_pages()
+
+secret = os.environ.get("SESSION_SECRET")
+ui.run(
+    host="127.0.0.1",
+    port=8080,
+    proxy_headers=True,
+    storage_secret=secret,
+    reload=False,
+    show=False,
+)
